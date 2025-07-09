9c3db30d4cf1f0a973b5d18adc99af2998d970a9
chingchen0518 <chingchen0518@gmail.com>
2025-07-09 23:24:21 +0800

    page 1 template

--- src/view2/chingchen/page1.jsx
+++ src/view2/chingchen/page1.jsx
@@ -6,13 +6,9 @@ import './page1.css';
 const Page1 = () => {
   return (
     <div className="page1">
-      {/* <div className="page1_header">
-        <h1 className="page1_title">旅遊規劃系統</h1>
-        <p className="page1_subtitle">選擇你喜愛的景點，規劃完美的旅程</p>
-      </div> */}
       <div className="page1_content">
-        <Schedule_container />
         <Attraction_container />
+        <Schedule_container />
       </div>
     </div>
   );

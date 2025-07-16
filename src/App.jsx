fd973d3a7c21bd4629920839975126e22679f09a
Tsai, Chia-Ching <jean700129@gmail.com>
2025-07-16 18:53:14 +0800

    更新成react

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Header/>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +32,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
     </>
   )
 }

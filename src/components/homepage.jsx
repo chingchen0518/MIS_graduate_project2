ddfdba2b030b7208cdbd457665493b88f3c5de60
chingchen0518 <153315236+chingchen0518@users.noreply.github.com>
2025-07-16 20:05:14 +0800

    update homepage

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import './homepage.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <a href="/attraction">attraction</a>
-      <br />
-      <a href="/page1">page1</a>
-      <br />
-      <a href="/map">map</a>
-      <br />
-      <a href="/MapDisplay">MapDisplay</a>
+    <div id ="home_page">
+      <ul>
+        <li><a href="/attraction">attraction</a></li>
+        <li><a href="/page1">page1</a></li>
+        <li><a href="/map">map</a></li>
+        <li><a href="/MapDisplay">MapDisplay</a></li>
+      </ul>
+     
+      
     </div>
 
   );

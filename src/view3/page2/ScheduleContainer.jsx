fdd60110d6d9132cb7ffe7dda34a4490995016f4
Nancyliu092 <sheila921224921224@gmail.com>
2025-08-27 18:39:25 +0800

    adjustTitleCSS

--- src/view3/page2/ScheduleContainer.jsx
+++ src/view3/page2/ScheduleContainer.jsx
@@ -138,7 +138,7 @@ const ScheduleContainer = ({ t_id, usedAttractions = [], onAttractionUsed, filte
     return (
         <div className="schedule_container">
             <div className="schedule_container_header">
-                <h2 className="schedule_container_title">旅遊行程</h2>
+                <h2 className="schedule_container_title">🚩旅遊行程</h2>
                 <div className="date-selector-wrapper">
                     <DateSelector
                         t_id={1} //@==@記得改掉@==@

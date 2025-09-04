13ad3bb42a3c4be33ad2aad63fc8707c5c559a54
chingchen0518 <chingchen0518@gmail.com>
2025-09-04 13:21:46 +0800

    change port to 3001

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173, // 前端開發用的埠號（可改可不改，預設也是 5173）
+    allowedHosts: [
+    'pointer-nu-participant-billion.trycloudflare.com',
+    '.trycloudflare.com', // 可加這行允許所有 cloudflare 子網域
+    'all'
+      ],
     proxy: {
       '/api': {
-        target: 'http://localhost:3999', // ← 你的後端 Express server
+        target: 'http://localhost:3001', // ← 你的後端 Express server
         changeOrigin: true,              // 修改 Host header，避免 CORS 問題
       },
     },
